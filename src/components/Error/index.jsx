315d4fe1d8637efb87bbe8a108798b71e27b3e9c
amarchive <antoine.marchive@orange.fr>
2024-08-12 23:01:46 +0200

    error component

---
+++ src/components/Error/index.jsx
@@ -0,0 +1,15 @@
+import './ErrorComponent.scss'
+
+export function Error () {
+    return (
+        <div className="error">
+            <p className="error__number">404</p>
+            <p className="error__text">
+                Oups! La page que vous demandez n'existe pas.
+            </p>
+            <Link className="error__link" to="/">
+                Retourner sur la page d'accueil
+            </Link>
+        </div>
+    )
+}
