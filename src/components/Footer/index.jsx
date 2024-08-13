edf2d4d78b87bce0562cc832756ec4e9aba621b1
amarchive <antoine.marchive@orange.fr>
2024-08-13 07:50:49 +0200

    modification img footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,11 @@
 import './Footer.scss'
-//import logo from './assets/footer__logo.png'
+import logo from '../../assets/footerLogo.png';
 
 export function Footer() {
     return (
         <footer className="footer">
             <p>ici est le footer</p>
+            <img className="footer__logo" src={logo} alt="Logo de l'agence immobilière Kasa"/>
             <p className="footer__copyrightText">
                 © 2020 Kasa. All rights reserved
             </p>
@@ -13,4 +14,3 @@ export function Footer() {
 }
 
 
-//<img className="footer__logo" src={logo} alt="Logo de l'agence immobilière Kasa"/>
