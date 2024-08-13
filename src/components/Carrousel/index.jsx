e917d63b02651c6c93b22acbab6e665f0ba2531c
amarchive <antoine.marchive@orange.fr>
2024-08-13 22:00:01 +0200

    delete carrousel

--- src/components/Carrousel/index.jsx
+++ src/components/Carrousel/index.jsx
@@ -1,12 +1,2 @@
 import './Carrousel.scss'
 
-function Carrousel({ img, className, classNameP }) {
-  return (
-    <div className={className}>
-      <img src={img} alt="Bannière paysage" />
-      <h1 className={classNameP}>{'Chez vous, \npartout et ailleurs'}</h1>
-    </div>
-  )
-}
-
-export default Carrousel
