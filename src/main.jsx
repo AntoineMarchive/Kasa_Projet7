adf9f1fd5bb44056a29f1d48ab822456b86370e0
amarchive <antoine.marchive@orange.fr>
2024-08-13 22:21:17 +0200

    logement page

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 //import { Home } from './pages/Home'
-import { Error } from './pages/Error'
+//import { Error } from './pages/Error'
+import { Logement } from './pages/Logement'
 import './index.scss'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Error></Error>
+    <Logement></Logement>
   </StrictMode>,
 )
--- src/pages/Logement/index.jsx
+++ src/pages/Logement/index.jsx
@@ -1,9 +1,19 @@
-import './Logement.scss'
+import { Header } from '../../components/Header';
+import { Footer } from '../../components/Footer';
+import { Carrousel } from '../../components/Carrousel';
+import { Thumb } from '../../components/Thumb';
+import { Tag } from '../../components/Tag';
+import './Logement.scss';
 
 export function Logement() {
     return (
         <main>
             <h1>Page Logement</h1>
+            <Header></Header>
+            <Carrousel></Carrousel>
+            <Tag></Tag>
+            <Thumb></Thumb>
+            <Footer></Footer>
         </main>
     )
 }
