dd367ad0648c5325bb01a26084bb0e9653b3fd0e
amarchive <antoine.marchive@orange.fr>
2024-08-13 08:37:27 +0200

    watch sass

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,14 +1,12 @@
 import './Banner.scss';
 import logo from "../../assets/homeBanner.png";
 
-function Banner() {
+export function Banner() {
     return (
         <div className="banner">
             <img className="banner__img" src={logo} alt="Bannière" />
             <div className="banner__darken"></div>
-            <span className="banner__text">{text}</span>
+            <span className="banner__text">Chez vous, partout et ailleurs</span>
         </div>
     )
 }
-
-export default Banner;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,6 @@
 import { Header } from '../../components/Header';
 import { Footer } from '../../components/Footer';
+import { Banner } from '../../components/Banner';
 import './Home.scss';
 
 
@@ -7,6 +8,7 @@ export function Home() {
     return (
         <main>
             <Header></Header>
+            <Banner></Banner>
             <h1>Page Home</h1>
             <Footer></Footer>
         </main>
