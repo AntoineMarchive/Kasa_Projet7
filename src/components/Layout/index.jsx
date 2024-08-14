548da2bfaa95714c91c9f998a821ef53ed65b768
amarchive <antoine.marchive@orange.fr>
2024-08-14 23:22:11 +0200

    create layout

---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,13 @@
+import { Header } from '../Header';
+import { Footer } from '../Footer';
+
+export function Layout () {
+
+    return(
+        <>
+        <Header />
+        <main></main>
+        <Footer />
+        </>
+    )
+}
