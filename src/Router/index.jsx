828c9bdf3cc93bd7cc27fc004b8d6676e98d27fc
amarchive <antoine.marchive@orange.fr>
2024-08-14 22:29:05 +0200

    router

--- src/Router/index.jsx
+++ src/Router/index.jsx
@@ -1,7 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
-import Home from "../pages/Home";
-import About from "../pages/About";
-import Logement from "../pages/Logement";
+import { Home } from "../pages/Home";
+import { About}  from "../pages/About";
+import { Logement } from "../pages/Logement";
+import { Error } from "../pages/Error";
 
 const router = createBrowserRouter([
   {
@@ -13,9 +14,13 @@ const router = createBrowserRouter([
     element: <About />,
   },
   {
-    path: "/logement",
+    path: "/logement/:logementId", // logement est lié a n'importe quoi, ceci est une variable//
     element: <Logement />,
   },
+  {
+    path: "*",
+    element: <Error />
+  }
 ]);
 
 export default router;
