07b316107f4a3603271f5c3d5257888f6e8a102d
amarchive <antoine.marchive@orange.fr>
2024-08-14 23:24:44 +0200

    don't find problems

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
-import './index.scss'
+import './index.scss';
 import { RouterProvider } from 'react-router-dom'
 import router from './Router'
 
