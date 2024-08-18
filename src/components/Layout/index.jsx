30a4caa4a2b19f2c36c3138ec469a3658f15e7b9
amarchive <antoine.marchive@orange.fr>
2024-08-18 18:10:33 +0200

    layout ok

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,12 +1,13 @@
 import { Header } from '../Header';
 import { Footer } from '../Footer';
+import { Children } from 'react';
 
 export function Layout () {
 
     return(
         <>
         <Header />
-        <main></main>
+            {Children}
         <Footer />
         </>
     )
