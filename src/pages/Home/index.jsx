4947608f8c561ebc5f13dc41557c0fcda7ca38d1
amarchive <antoine.marchive@orange.fr>
2024-08-19 09:45:13 +0200

    modification Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,31 +1,46 @@
 import { useEffect, useState } from "react";
+import { Layout } from "../../components/Layout";
 import { Banner } from "../../components/Banner";
 import { Thumb } from "../../components/Thumb";
 import "./Home.scss";
 
-export default function Home() {
+export function Home() {
   const [logements, setLogements] = useState([]); //hook de react//
 
   useEffect(() => {
     async function fetchLogements() {
+      try {
       const res = await fetch("/logements.json");
+      if (!res.ok) {
+        throw new Error("La réponse du serveur n'est pas bonne")
+      }
       const datas = await res.json();
       setLogements(datas);
+    } catch (error) { 
+      console.error("Fetch error: ", error);
     }
-
+  }
     fetchLogements();
   }, []);
 
   return (
-    <main>
-      <Banner></Banner>
-      <section className="thumbs">
-        {logements.map((logement) => (
-          <Thumb key={logement.id}
-          title={logement.title}
-          cover={logement.cover} />
-        ))}
-      </section>
-    </main>
+    <Layout>
+      <main>
+        <Banner></Banner>
+        <section className="thumbs">
+          {logements.length < 0 ? (
+          logements.map((logement) => (
+            <Thumb
+              key={logement.id}
+              title={logement.title}
+              cover={logement.cover}
+            />
+          ))
+  ) : (
+    <p>Loading...</p>
+  )}
+        </section>
+      </main>
+    </Layout>
   );
 }
