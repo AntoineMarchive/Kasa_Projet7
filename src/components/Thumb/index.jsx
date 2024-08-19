99c5d9d2f99c2b2d5c4f3b61bbc8393353e5d3fc
amarchive <antoine.marchive@orange.fr>
2024-08-19 14:43:10 +0200

    modification thumb component

--- src/components/Thumb/index.jsx
+++ src/components/Thumb/index.jsx
@@ -1,10 +1,12 @@
+import React from "react";
 import "./Thumb.scss";
 
 
-export function Thumb() {
+export function Thumb({ image, title }) {
   return (
-    <div className="thumb">
-      <h2 className="thumb__title"> Titre de la location</h2>
-    </div>
+    <li className="thumb">
+      <img className="thumb__image" src={image} alt="" ></img>
+      <h2 className="thumb__title"> {title}</h2>
+    </li>
   );
 }
