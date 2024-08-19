3488d0d62cbe52bfb06aba034afd806dab74d3d4
amarchive <antoine.marchive@orange.fr>
2024-08-19 09:44:13 +0200

    complete logement

--- src/pages/Logement/index.jsx
+++ src/pages/Logement/index.jsx
@@ -1,25 +1,38 @@
-import { Header } from "../../components/Header";
-import { Footer } from "../../components/Footer";
 import { Carrousel } from "../../components/Carrousel";
 import { Collapse } from "../../components/Collapse";
 import { Tag } from "../../components/Tag";
+import { Layout } from "../../components/Layout";
 import "./Logement.scss";
-import { useParams } from "react-router-dom";
+import { useParams, NavLink } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export function Logement() {
   const { logementId } = useParams();
+  const [logement, setLogement] = useState();
+
+  useEffect(() => {
+    async function fetchLogements() {
+      const res = await fetch("/logements.json");
+      const datas = await res.json();
+
+      const foundLogement = datas.find((log) => {
+        return log.id === logementId;
+      })
+
+      if(foundLogement) {
+        setLogement(foundLogement);
+      } else {
+      }
+  
+    }
+    fetchLogements();
+  }, [logementId]);
+
   return (
-    <main>
-      <h1>Page Logement {logementId}</h1>
-      <Header></Header>
-      <Carrousel></Carrousel>
-      <Tag></Tag>
-      <Tag></Tag>
-      <Tag></Tag>
-      <Collapse></Collapse>
-      <Collapse></Collapse>
-      <Footer></Footer>
-    </main>
+    <Layout>
+      <main>
+        <h1>Page Logement {logementId}</h1>
+      </main>
+    </Layout>
   );
 }
-
