09f191f6237b23f3275878ea8cc174284f6363aa
amarchive <antoine.marchive@orange.fr>
2024-08-19 09:44:57 +0200

    modification about

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,20 +1,21 @@
-import { Header } from '../../components/Header';
-import { Footer } from '../../components/Footer';
-import { Collapse } from '../../components/Collapse';
-import { Banner } from '../../components/Banner';
-import './About.scss'
+//import { Header } from '../../components/Header';
+//import { Footer } from '../../components/Footer';
+import { Collapse } from "../../components/Collapse";
+import { Banner } from "../../components/Banner";
+import { Layout } from "../../components/Layout";
+import "./About.scss";
 
 export function About() {
-    return (
-        <main>
-            <h1>Page About</h1>
-            <Header></Header>
-            <Banner></Banner>
-            <Collapse></Collapse>
-            <Collapse></Collapse>
-            <Collapse></Collapse>
-            <Collapse></Collapse>
-            <Footer></Footer>
-        </main>
-    )
+  return (
+    <Layout>
+      <main>
+        <h1>Page About</h1>
+        <Banner></Banner>
+        <Collapse></Collapse>
+        <Collapse></Collapse>
+        <Collapse></Collapse>
+        <Collapse></Collapse>
+      </main>
+    </Layout>
+  );
 }
