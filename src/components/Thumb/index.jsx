b278ceffb0d1cd7cc537f040e10d7a212676dcc2
amarchive <antoine.marchive@orange.fr>
2024-08-20 13:36:58 +0200

    modif thumb

--- src/components/Thumb/index.jsx
+++ src/components/Thumb/index.jsx
@@ -5,7 +5,7 @@ import "./Thumb.scss";
 export function Thumb({ cover, title }) {
   return (
     <li className="thumb">
-      <img className="thumb__image" src={cover} alt="" ></img>
+      <img className="thumb__image" src={cover} alt={cover} ></img>
       <h2 className="thumb__title"> {title}</h2>
     </li>
   );
