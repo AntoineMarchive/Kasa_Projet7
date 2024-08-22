709fc50b654ad84698881b7bef1e7b9f6c0493ee
amarchive <antoine.marchive@orange.fr>
2024-08-22 23:35:05 +0200

    correction banner

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import './Banner.scss';
 import Logo from '../../assets/homeBanner.png';
 
-export function Banner({ logo = Logo, text }) {
+export function Banner({ logo = Logo, children }) {
   return (
     <div className="banner">
       <img className="banner__img" src={logo} alt="Bannière" />
       <div className="banner__darken"></div>
-      <span className="banner__text">{text}</span>
+      {children}
     </div>
   );
 }
