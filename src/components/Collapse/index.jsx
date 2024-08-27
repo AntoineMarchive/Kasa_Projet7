86ee9577f57485f137c222a3a5b1c6abc851cb79
amarchive <antoine.marchive@orange.fr>
2024-08-27 22:01:36 +0200

    PropTypes

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,35 +1,37 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import './Collapse.scss';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import "./Collapse.scss";
 
 export function Collapse({ title, children }) {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleCollapse = () => {
-        setIsOpen(!isOpen);
-    };
+  const toggleCollapse = () => {
+    setIsOpen(!isOpen);
+  };
 
-    return (
-        <header className="collapse-container">
-            <div className="collapse">
-                <button
-                    className={`collapse__toggle ${isOpen ? 'open' : 'closed'}`}
-                    onClick={toggleCollapse}
-                >
-                    {title}
-                    <i
-                        className={`collapse__icon fas fa-chevron-up ${isOpen ? 'open' : 'closed'}`}
-                    ></i>
-                </button>
-                <div className={`collapse__content ${isOpen ? 'open' : 'closed'}`}>
-                    {children}
-                </div>
-            </div>
-        </header>
-    );
+  return (
+    <header className="collapse-container">
+      <div className="collapse">
+        <button
+          className={`collapse__toggle ${isOpen ? "open" : "closed"}`}
+          onClick={toggleCollapse}
+        >
+          {title}
+          <i
+            className={`collapse__icon fas fa-chevron-up ${
+              isOpen ? "open" : "closed"
+            }`}
+          ></i>
+        </button>
+        <div className={`collapse__content ${isOpen ? "open" : "closed"}`}>
+          {children}
+        </div>
+      </div>
+    </header>
+  );
 }
 
 Collapse.propTypes = {
-    title: PropTypes.string.isRequired,
-    children: PropTypes.node.isRequired,
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
 };
--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,14 +1,18 @@
-import { Header } from '../Header';
-import { Footer } from '../Footer';
-import { Children } from 'react';
+import { Header } from "../Header";
+import { Footer } from "../Footer";
+import { Children } from "react";
+import PropTypes from "prop-types";
 
-export function Layout ({ children }) {
-
-    return(
-        <>
-        <Header />
-            {children}
-        <Footer />
-        </>
-    );
+export function Layout({ children }) {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
 }
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
