82e477b2aa35b064d413f39578ed5e8b42bed334
amarchive <antoine.marchive@orange.fr>
2024-08-31 12:48:17 +0200

    correction carrousel

--- src/components/Carrousel/index.jsx
+++ src/components/Carrousel/index.jsx
@@ -16,27 +16,35 @@ export function Carrousel({ images }) {
       prevIndex === images.length - 1 ? 0 : prevIndex + 1
     );
   };
+  
+const hasMultipleImages = images.length > 1;
 
   return (
     <div className="carrousel">
+      {hasMultipleImages && (
       <button
         className="carrousel__arrow carrousel__arrow--left"
         onClick={handlePrev}
       >
         <i className="fa-solid fa-chevron-left"></i>
       </button>
+      )}
       <div className="carrousel__images">
         <img src={images[currentIndex]} alt={`Slide ${currentIndex + 1}`} />
       </div>
+      {hasMultipleImages &&(
       <button
         className="carrousel__arrow carrousel__arrow--right"
         onClick={handleNext}
       >
         <i className="fa-solid fa-chevron-right"></i>
       </button>
+      )}
+      {hasMultipleImages && (
       <div className="carrousel__count">
         {currentIndex + 1} / {images.length}
       </div>
+      )}
     </div>
   );
 }
